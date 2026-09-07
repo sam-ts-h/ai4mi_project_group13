432523a5bea29b606483bc9f4d51e698aed22d48
Fay de Rijk <fayderijk@MacBook-Air-2.local>
2026-09-07 11:15:11 +0200

    Allow stitching when not all classes are present

--- stitch.py
+++ stitch.py
@@ -73,7 +73,8 @@ def merge_patient(id_: str, dest_folder: str, images: list[Path],
 
     # res_arr = res_arr.astype(np.int16)
     res_arr //= 63  # For segthor only
-    assert set(np.unique(res_arr)) == set(range(5)), np.uint8(res_arr)
+    assert set(np.unique(res_arr)).issubset(set(range(5))), np.uint8(res_arr) ##aangepast van == set(range(5)) naar .issubset(set(range(5)))
+    #assert set(np.unique(res_arr)) == set(range(5)), np.uint8(res_arr) #old sentence 
 
     new_nib = nib.nifti1.Nifti1Image(res_arr, affine=orig_nib.affine, header=orig_nib.header)
     nib.save(new_nib, (Path(dest_folder) / id_).with_suffix(".nii.gz"))
